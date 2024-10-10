92175d68c9393a8e2e40de0dc0a9d48591cf7a86
clinicabonnadona <administracion@clinicabonnadona.com>
2024-10-10 09:32:02 -0500

    viteConfig 10/10/2024 - 4

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
 import {createRouter, createWebHistory} from "vue-router";
 
-import {routesObject} from "./routes";
+import {routesObject} from "./routes/index.ts";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
